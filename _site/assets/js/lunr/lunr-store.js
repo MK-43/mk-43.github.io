62151b8c79e3c1562b87029d9ae304699a46fd77
mk-43 <mkk043@gmail.com>
2022-04-19 12:36:49 +0900

    Create post about fd and some few modifications on files

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Fd",
+        "excerpt":"Find directories only  fd -t d file_name  ","categories": [],
+        "tags": [],
+        "url": "/fd/",
+        "teaser": null
+      }]
