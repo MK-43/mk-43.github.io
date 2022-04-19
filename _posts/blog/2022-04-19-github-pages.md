eb7db4acac4d9aaac18b65d685551aeb9014d8ed
mk-43 <mkk043@gmail.com>
2022-04-19 20:16:10 +0900

    Add sidebar and other posts

---
+++ _posts/blog/2022-04-19-github-pages.md
@@ -0,0 +1,30 @@
+---
+title:  "[github pages] overall"
+excerpt: "Describe how to create github pages"
+
+categories:
+  - jekyll
+tags:
+  - [blog]
+
+toc: true
+toc_sticky: true
+ 
+date: 2022-04-19
+last_modified_at: 2022-04-19
+---
+
+### _posts
+포스트는 바로 최상단 아래 `_post` 카테고리에 존재해야 함.
+없으면 새로 만든다.
+
+### _config.yml
+Settings that affect your entire site.
+
+### YAML Front Matter
+_config.yml 아래에 있는 `defaults`를 말하는 듯
+
+Using the `default` key in `_config.yml` you could set the layout for all posts
+
+
+각각 개별 _configyml파일을 생성한다.
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,31 @@
 var store = [{
-        "title": "Fd",
-        "excerpt":"Find directories only  fd -t d file_name  ","categories": [],
-        "tags": [],
-        "url": "/fd/",
+        "title": "[aws] s3",
+        "excerpt":"  ","categories": ["aws"],
+        "tags": ["s3"],
+        "url": "/aws/s3/",
+        "teaser": null
+      },{
+        "title": "[github pages] overall",
+        "excerpt":"_posts  포스트는 바로 최상단 아래 _post 카테고리에 존재해야 함. 없으면 새로 만든다.   _config.yml  Settings that affect your entire site.   YAML Front Matter  _config.yml 아래에 있는 defaults를 말하는 듯   Using the default key in _config.yml you could set the layout for all posts   각각 개별 _configyml파일을 생성한다.  ","categories": ["jekyll"],
+        "tags": ["blog"],
+        "url": "/jekyll/github-pages/",
+        "teaser": null
+      },{
+        "title": "[fd] commands",
+        "excerpt":"Find directories only   fd -t d file_name  ","categories": ["linux"],
+        "tags": ["fd"],
+        "url": "/linux/fd/",
+        "teaser": null
+      },{
+        "title": "[Web] html tag",
+        "excerpt":"span is used to color a part of a text   &lt;p&gt;Ny mother has &lt;span style=\"color:blue\"&gt;blue&lt;/span&gt; eyes.&lt;p&gt;  ","categories": ["web"],
+        "tags": ["html"],
+        "url": "/web/html-tag/",
+        "teaser": null
+      },{
+        "title": "[Web] markdown",
+        "excerpt":"inline: to denote a word or phrase as code, enclose it in backticks.  ","categories": ["web"],
+        "tags": ["markdown"],
+        "url": "/web/markdown/",
         "teaser": null
       }]
