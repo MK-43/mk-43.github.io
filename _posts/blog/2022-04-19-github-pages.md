208f03e797b770db9e615bfd0a080c0e1ee0256f
mk-43 <mkk043@gmail.com>
2022-04-20 21:13:28 +0900

    TIL 2022-04-20

--- _posts/blog/2022-04-19-github-pages.md
+++ _posts/blog/2022-04-19-github-pages.md
@@ -15,16 +15,27 @@ last_modified_at: 2022-04-19
 ---
 
 ### _posts
+
 포스트는 바로 최상단 아래 `_post` 카테고리에 존재해야 함.
 없으면 새로 만든다.
 
 ### _config.yml
+
 Settings that affect your entire site.
 
 ### YAML Front Matter
+
 _config.yml 아래에 있는 `defaults`를 말하는 듯
+any default value can be overridden by settings in a post, page, or collection file.
 
 Using the `default` key in `_config.yml` you could set the layout for all posts
 
-
 각각 개별 _configyml파일을 생성한다.
+
+## jekyll
+
+run jekyll server with my IP
+
+```bash
+bundle exec jekyll serve --host={my_ip}
+```
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [{
         "teaser": null
       },{
         "title": "[github pages] overall",
-        "excerpt":"_posts  포스트는 바로 최상단 아래 _post 카테고리에 존재해야 함. 없으면 새로 만든다.   _config.yml  Settings that affect your entire site.   YAML Front Matter  _config.yml 아래에 있는 defaults를 말하는 듯   Using the default key in _config.yml you could set the layout for all posts   각각 개별 _configyml파일을 생성한다.  ","categories": ["jekyll"],
+        "excerpt":"_posts 포스트는 바로 최상단 아래 _post 카테고리에 존재해야 함. 없으면 새로 만든다. _config.yml Settings that affect your entire site. YAML Front Matter _config.yml 아래에 있는 defaults를 말하는 듯 any default value can be overridden by settings in a post, page, or collection file. Using the default key in _config.yml you...","categories": ["jekyll"],
         "tags": ["blog"],
         "url": "/jekyll/github-pages/",
         "teaser": null
@@ -33,5 +33,23 @@ var store = [{
         "excerpt":"inline: to denote a word or phrase as code, enclose it in backticks.  ","categories": ["web"],
         "tags": ["markdown"],
         "url": "/web/markdown/",
+        "teaser": null
+      },{
+        "title": "[bash] overview",
+        "excerpt":"declare variable declare set variable values and attributes In most cases, it’s enought with an implicit declartion in bash my_var=\"some value\" But, sometimes you want a variable’s value to only be integer. declare -i num=15 declare -r # readonly, but local scope readonly the default scope is global readonly b=1...","categories": ["linux"],
+        "tags": ["bash"],
+        "url": "/linux/bash-script/",
+        "teaser": null
+      },{
+        "title": "[tmux] overview",
+        "excerpt":"a terminal multiplexer install sudo apt install tmux tmux ctrl+b ? list up all commands tmux new -s session creat a new named session window ctrl+b d detach from the Tmux session ctrl+b n go to the next window ctrl+b p go to the previous window pane ctrl+b ; go...","categories": ["linux"],
+        "tags": ["tmux"],
+        "url": "/linux/tmux/",
+        "teaser": null
+      },{
+        "title": "[vscode] overview",
+        "excerpt":"ssh-remote   extension must be installed in remote vscode.  ","categories": [null,""],
+        "tags": ["vscode"],
+        "url": "/vscode/",
         "teaser": null
       }]
